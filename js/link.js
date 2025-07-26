954092c3ef11f27cfbee345a36099ed5d56d9f6d
Alok Kumar <136910791+alokkumar777@users.noreply.github.com>
2025-07-26 23:15:32 +0530

    feat: dynamically load product, hero, and footer

--- js/link.js
+++ js/link.js
@@ -4,18 +4,21 @@ fetch("./navbar.html")
   .then((data) => {
     document.getElementById("navbar").innerHTML = data;
   });
-
-//   Product
+// Product
 fetch("./product.html")
   .then((res) => res.text())
   .then((data) => {
     document.getElementById("product").innerHTML = data;
   });
+
+// Hero
 fetch("./hero.html")
   .then((res) => res.text())
   .then((data) => {
     document.getElementById("hero").innerHTML = data;
   });
+
+// Footer
 fetch("./footer.html")
   .then((res) => res.text())
   .then((data) => {
